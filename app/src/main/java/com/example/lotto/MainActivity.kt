1c4343ef8e680e5f23d85de59e68c3c725168256
82103 <chjw9806@naver.com>
2021-05-26 13:25:11 +0900

    Random Number Generation

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -6,14 +6,60 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+fun getRandomLottoNumber() : Int{
+    return Random().nextInt(45)+1
+}
+fun getRandomLottoNumbers() : MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while (true){
+        var number: Int = getRandomLottoNumber()
+        var flag_existing: Int = 0
+
+        if(lottoNumbers.contains(number)){
+            //flag_existing=1
+            continue
+
+   }
+       /* if(flag_existing.equals(1))
+            continue
+        else*/
+        lottoNumbers.add(number)
+        if(lottoNumbers.size >= 6)
+            break;
+        //lottoNumbers.add(getRandomLottoNumber())
+    }
+    return lottoNumbers
+}
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val intent = findViewById<CardView>(R.id.nameCard)
-        findViewById<CardView>(R.id.randomCard).setOnClickListener {
+        val cardRandom = findViewById<CardView>(R.id.randomCard)
+        cardRandom.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
+        }
+
+        val cardConstell = findViewById<CardView>(R.id.ConstellationCard)
+        cardConstell.setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+
+        val cardName = findViewById<CardView>(R.id.nameCard)
+        cardName.setOnClickListener {
+            startActivity(Intent(this, NameActivity::class.java))
+        }
+
+        /*findViewById<CardView>(R.id.randomCard).setOnClickListener {
             startActivity(Intent(this, ResultActivity::class.java))
         }
 
@@ -24,6 +70,6 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<CardView>(R.id.nameCard).setOnClickListener {
             startActivity(Intent(this, NameActivity::class.java))
-        }
+        }*/
     }
 }
